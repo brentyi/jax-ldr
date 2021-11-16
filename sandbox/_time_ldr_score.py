780347d883c067045b5d1308203bb974f0213885
Brent Yi <yibrenth@gmail.com>
2021-11-16 11:11:39 -0800

    Sync some more runtime experiments

--- sandbox/_time_ldr_score.py
+++ sandbox/_time_ldr_score.py
@@ -1,14 +1,16 @@
 import sys
+from tqdm.auto import tqdm
 
 sys.path.append("..")
+
 import functools
 import math
 import timeit
 
 import jax
 import numpy as onp
-
 from ldr import ldr
+from matplotlib import pyplot as plt
 
 
 def make_one_hot(labels: onp.ndarray, num_classes: int) -> onp.ndarray:
@@ -19,45 +21,61 @@ def make_one_hot(labels: onp.ndarray, num_classes: int) -> onp.ndarray:
     return out_flat.reshape(labels.shape + (num_classes,))
 
 
-def f(theta, Z, one_hot_labels, use_vmap):
+def f(theta, Z, one_hot_labels, vectorize):
     Z = Z * theta
     Z_hat = Z + theta
     a, b, c = ldr.ldr_score_terms(
-        Z, Z_hat, one_hot_labels=one_hot_labels, epsilon_sq=0.5, use_vmap=use_vmap
+        Z,
+        Z_hat,
+        one_hot_labels=one_hot_labels,
+        epsilon_sq=0.5,
+        vectorize_over_classes=vectorize,
     )
     return a + b + c
 
 
-f_vmap = functools.partial(f, use_vmap=True)
-f_loop = functools.partial(f, use_vmap=False)
+f_vmap = functools.partial(f, vectorize=True)
+f_loop = functools.partial(f, vectorize=False)
 
-
-K = 10  # Number of classes. One coding rate computed for each class.
-N = 2048  # Batch size.
-D = 128  # Latent dimension.
-
-Z = onp.random.randn(N, D)
-one_hot_labels = make_one_hot(
-    onp.random.randint(low=0, high=K, size=(N,)), num_classes=K
-)
-assert one_hot_labels.shape == (N, K)
 
 funcs = {
     "vmap": f_vmap,
     "loop": f_loop,
 }
 
-print("FORWARD PASS")
+K: int  # Number of classes. One coding rate computed for each class.
 for name, f in funcs.items():
-    theta = onp.random.randn(128)
-    jit_f = jax.jit(f)
-    jit_f(theta, Z, one_hot_labels)
-    print(name, timeit.timeit(lambda: jit_f(theta, Z, one_hot_labels), number=100))
+    Ks = range(5, 51, 5)
+    runtimes = []
+    for K in tqdm(Ks):
+        N = 2048  # Batch size.
+        D = 128  # Latent dimension.
 
-print()
-print("BACKWARD PASS")
-for name, f in funcs.items():
-    theta = onp.random.randn(128)
-    jit_f = jax.jit(jax.grad(f))  # type: ignore
-    jit_f(theta, Z, one_hot_labels)
-    print(name, timeit.timeit(lambda: jit_f(theta, Z, one_hot_labels), number=100))
+        Z = onp.random.randn(N, D)
+        one_hot_labels = make_one_hot(
+            onp.random.randint(low=0, high=K, size=(N,)), num_classes=K
+        )
+        assert one_hot_labels.shape == (N, K)
+        # print("FORWARD PASS")
+        time = []
+        theta = onp.random.randn(D)
+        jit_f = jax.jit(jax.grad(f))
+        jit_f(theta, Z, one_hot_labels)
+        runtimes.append(timeit.timeit(lambda: jit_f(theta, Z, one_hot_labels), number=100))
+    plt.plot(Ks, runtimes, label=name)
+
+plt.xlabel("K (number of classes)")
+plt.ylabel("seconds for 100 runs")
+plt.legend()
+plt.ylim(bottom=0)
+plt.title("LDR score backward pass time")
+plt.show()
+
+
+# print()
+# print("BACKWARD PASS")
+# for name, f in funcs.items():
+#     theta = onp.random.randn(128)
+#     jit_f = jax.jit(jax.grad(f))  # type: ignore
+#     jit_f(theta, Z, one_hot_labels)
+#     print(name, timeit.timeit(lambda: jit_f(theta, Z, one_hot_labels), number=100))
---
+++ sandbox/_time_logdet.py
@@ -0,0 +1,48 @@
+import timeit
+
+import jax
+import numpy as onp
+from jax import numpy as jnp
+from matplotlib import pyplot as plt
+from tqdm.auto import tqdm
+
+
+def logdet_hermitian(A: jnp.ndarray) -> float:
+    return 2 * jnp.sum(jnp.log(jnp.diag(jnp.linalg.cholesky(A))))
+
+
+jax_logdets = {
+    "standard logdet": lambda A: jnp.linalg.slogdet(A)[1],
+    "cholesky logdet": lambda A: 2 * jnp.sum(jnp.log(jnp.diag(jnp.linalg.cholesky(A)))),
+}
+# jax_logdets = {k: jax.jit(v) for k, v in jax_logdets.items()}
+
+number: int = 1000
+
+fig, axes = plt.subplots(1, 2)
+
+for name, f_raw in jax_logdets.items():
+    for f, ax in zip((jax.jit(f_raw), jax.jit(jax.grad(f_raw))), axes):
+        dims = range(1, 200, 10)
+        times = []
+        for n in tqdm(dims):
+            A = onp.random.randn(n, n)
+            A = A @ A.T
+            f(A)
+            times.append(timeit.timeit(lambda: f(A), number=number))
+        ax.plot(dims, times, label=name)
+
+ax0, ax1 = axes
+ax0.set_ylim(bottom=0)
+ax0.legend()
+ax0.set_title("Forward pass logdet runtimes")
+ax0.set_xlabel("N (matrix dim)")
+ax0.set_ylabel(f"seconds for {number} calls")
+
+ax1.set_ylim(bottom=0)
+ax1.legend()
+ax1.set_title("Backward pass logdet runtimes")
+ax1.set_xlabel("N (matrix dim)")
+ax1.set_ylabel(f"seconds for {number} calls")
+
+plt.show()
